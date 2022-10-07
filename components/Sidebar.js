afba795f0779f5f826a594971a17ef53322a4de1
Walter Zhang <walter.zhangfang@gmail.com>
2022-10-07 15:52:48 +1300

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -63,13 +63,13 @@ export default function Sidebar() {
         </div>
       </div>
 
-      <div className="card mb-4">
+      {/* <div className="card mb-4">
         <div className="card-header">Side Widget</div>
         <div className="card-body">
           You can put anything you want inside of these side widgets. They are
           easy to use, and feature the Bootstrap 5 card component!
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
